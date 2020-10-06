216195955f30283b927c97599f321a5c18b19236
Reza Aditya Sutrisno <adityareza46@gmail.com>
2020-10-06 10:29:42 +0700

    Attaching Products to Users & Filtering By Creator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -73,9 +73,11 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> fetchAndSetProducts() async {
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString =
+        filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
     var url =
-        'https://elite-vista-203713.firebaseio.com/products.json?auth=$authToken';
+        'https://elite-vista-203713.firebaseio.com/products.json?auth=$authToken&$filterString';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -116,6 +118,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
         }),
       );
       final newProduct = Product(
